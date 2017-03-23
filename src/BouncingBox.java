261304b1edf4cd60f4c991d92c3799df79395b1e
Sebastian Cifredo <Sebastian Cifredo@Sebas-PC>
2017-03-23 12:36:45 -0400

    a

---
+++ src/BouncingBox.java
@@ -0,0 +1,19 @@
+import java.awt.Graphics;
+import java.util.ArrayList;
+import javax.swing.JComponent;
+
+public class BouncingBox extends JComponent {
+
+	private ArrayList<BouncingFigure> figures = new ArrayList<BouncingFigure>();
+
+	public void add(BouncingFigure f) {
+		figures.add(f);
+	}
+
+	public void paintComponent(Graphics g) {
+		for (BouncingFigure f : figures) {
+			f.draw(g); f.move();
+			
+		}
+	}
+}
